2b5b9c85df80984170093fcbb96ca05c791e8544
Michael Wood <michael.g.wood@intel.com>
2012-11-26 15:01:44 +0000

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Installation module for Dawati User Testing.
 from distutils.core import setup
 
 setup(name='Dawati user testing',
-      version='0.3',
+      version='0.4',
       description='Tool for creating observation videos of users performing tasks on desktop applications or other devices',
       author=file('AUTHORS').read(),
       url='http://github.com/dawati/dawati-user-testing',
